cd05d81fcab4d8831fb11497716da9c4eeb44b98
Narendra-150 <narendrabagul84@gmail.com>
2022-02-01 23:12:45 +0530

    smallApps - log updated in  csv-file-process...

--- csv-files-process/replace-in-csv-concurrent/app.js
+++ csv-files-process/replace-in-csv-concurrent/app.js
@@ -1,35 +1,5 @@
 const process = require('./logic/process');
+const log = require('../helper/log');
 
-
-// const axios = require('axios');
-
-// function countryFromIp(ip) {
-//   return axios.get(`http://ip-api.com/json/${ip}`)
-//     .then(res => res.data.country);
-// }
-
-// const promiseMap = (promiseArray, inputFunction)=> {
-// 	const reducer = (accumulator, currentItem) => {
-//   return accumulator.then((_finalResult) => {
-//     return inputFunction(currentItem).then((_result) => {
-//       _finalResult[currentItem] = _result;
-// 				return _finalResult;
-// 			})}
-// 		);
-//     }
-// 	return promiseArray.reduce(reducer, Promise.resolve({}));
-// }
-
-// promiseMap([ '8.8.8.8', '41.182.194.0', '5.34.159.1' ], countryFromIp)//.then(console.log);
-
-
-
-// function promiseMap(xs, f) {
-//   const reducer = (ysAcc$, x) =>
-//   ysAcc$.then(ysAcc => f(x).then(y => { ysAcc[x] = y; return ysAcc; }));
-//   return xs.reduce(reducer, Promise.resolve({}));
-//   }
-  
-//   promiseMap(["8.8.8.8", "41.182.194.0", "5.34.159.1"], countryFromIp).then(console.log);
-
+log('App started...');
 process();
